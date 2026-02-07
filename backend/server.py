bd4ba544bc6c091d4aa07e0b3de2b31b4a8cb08d
Sameersingh29 <sameerchandra12@gmail.com>
2026-02-07 18:39:10 +0530

    Update CORRS

--- backend/server.py
+++ backend/server.py
@@ -329,7 +329,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["https://code-and-canvas.vercel.app","*"],
+    allow_origins=["https://code-and-canvas.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
